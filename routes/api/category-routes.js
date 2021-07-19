2ebb14dc10ca296fed45cd9426c3be455e83cdcc
Blenda Orellana <blen.or90@gmail.com>
2021-07-18 23:13:00 -0500

    fixed routes and functions to match one another

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -17,10 +17,10 @@ router.get('/', async (req, res) => {
 });
 
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   try {
-    const categoryData = await.Category.findByPk(req.params.id, {
+    const categoryData = await Category.findByPk(req.params.id, {
       // be sure to include its associated Products
       include: [{ model: Product }],
     });
@@ -46,6 +46,7 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
   const categoryData = await Category.update(
     {
       category_name: req.body.category_name
@@ -56,19 +57,35 @@ router.put('/:id', async (req, res) => {
       },
     }
   );
-  return res.json(categoryData);
+  if (!categoryData) {
+    res.status(404).json({ message: 'No category found with that id!' });
+    return;
+  }
+
+  res.status(200).json(categoryData);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try{
   const categoryData = await Category.destroy({
       where: {
         id: req.params.id,
       }
     });
 
-  return res.json(categoryData);
-});
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
 
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -25,11 +25,11 @@ router.get('/:id', async (req, res) => {
       // be sure to include its associated Category and Tag data
 
       include: [
-        { model: Tag, through: ProductTag},
-        {model: Category},
+        { model: Tag, through: ProductTag },
+        { model: Category },
       ]
     });
-      
+
 
     if (!productData) {
       res.status(404).json({ message: 'No product found with that id!' });
@@ -42,10 +42,10 @@ router.get('/:id', async (req, res) => {
   }
 
 });
- 
+
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -77,7 +77,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -118,15 +118,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
-  const productData = await Product.destroy({
-    where: {
-      id: req.params.id,
+  try{
+    const productData = await Product.destroy({
+        where: {
+          id: req.params.id,
+        }
+      });
+  
+      if (!productData) {
+        res.status(404).json({ message: 'No product found with that id!' });
+        return;
+      }
+  
+      res.status(200).json(productData);
+    } catch (err) {
+      res.status(500).json(err);
     }
-  });
-
-return res.json(productData);
 });
 
 module.exports = router;
